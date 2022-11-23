16ffc6910266705416f25ad0aa18d02234e11462
subhash-int <118886661+subhash-int@users.noreply.github.com>
2022-11-23 14:23:31 +0530

    Add files via upload
    Detecting leaves and greens using sample test images found on the net

---
+++ workingdraft-leaf1.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+## Read
+img = cv2.imread("test3.jpeg")
+
+## convert to hsv
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+## mask of green (36,25,25) ~ (86, 255,255)
+mask = cv2.inRange(hsv, (36, 25, 25), (86, 255, 255))
+# mask = cv2.inRange(hsv, (36, 25, 25), (70, 255,255))
+
+## slice the green
+imask = mask > 0
+green = np.zeros_like(img, np.uint8)
+green[imask] = img[imask]
+
+## save
+cv2.imshow("Image INPUT", img)
+cv2.imshow("Image", green)
+cv2.waitKey()
+cv2.destoryAllWindows()
